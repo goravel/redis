d3d12389ce945c284f9c2e3d06d13c3ab86c7b6b
Bowen <hwbrzzl@gmail.com>
2025-04-08 20:11:45 +0800

    recover queue arg

--- queue.go
+++ queue.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/gob"
-	"errors"
 	"fmt"
 	"strconv"
 	"time"
@@ -12,15 +11,12 @@ import (
 	"github.com/redis/go-redis/v9"
 
 	"github.com/goravel/framework/contracts/config"
+	"github.com/goravel/framework/contracts/foundation"
 	"github.com/goravel/framework/contracts/queue"
 	frameworkerrors "github.com/goravel/framework/errors"
 
 	supportredis "github.com/goravel/redis/support/redis"
 )
-
-func init() {
-	gob.Register(queueData{})
-}
 
 type queueData struct {
 	Signature string `json:"signature"`
@@ -37,7 +33,7 @@ type Queue struct {
 	connection string
 }
 
-func NewQueue(ctx context.Context, config config.Config, queue queue.Queue, connection string) (*Queue, error) {
+func NewQueue(ctx context.Context, config config.Config, queue queue.Queue, json foundation.Json, connection string) (*Queue, error) {
 	connection = config.GetString(fmt.Sprintf("queue.connections.%s.connection", connection), "default")
 
 	client, err := supportredis.GetClient(config, connection)
@@ -85,14 +81,14 @@ func (r *Queue) Driver() string {
 	return queue.DriverCustom
 }
 
-func (r *Queue) Pop(queue string) (queue.Job, []any, error) {
+func (r *Queue) Pop(queue string) (queue.Job, []queue.Arg, error) {
 	if err := r.migrateDelayedJobs(queue); err != nil {
 		return nil, nil, err
 	}
 
 	result, err := r.instance.LPop(r.ctx, queue).Bytes()
 	if err != nil {
-		if errors.Is(err, redis.Nil) {
+		if frameworkerrors.Is(err, redis.Nil) {
 			return nil, nil, frameworkerrors.QueueDriverNoJobFound.Args(queue)
 		}
 		return nil, nil, err
@@ -110,7 +106,7 @@ func (r *Queue) Pop(queue string) (queue.Job, []any, error) {
 	return job, args, nil
 }
 
-func (r *Queue) Push(job queue.Job, args []any, queue string) error {
+func (r *Queue) Push(job queue.Job, args []queue.Arg, queue string) error {
 	payload, err := r.jobToGob(job.Signature(), args)
 	if err != nil {
 		return err
@@ -119,7 +115,7 @@ func (r *Queue) Push(job queue.Job, args []any, queue string) error {
 	return r.instance.RPush(r.ctx, queue, payload).Err()
 }
 
-func (r *Queue) Later(delay time.Time, job queue.Job, args []any, queue string) error {
+func (r *Queue) Later(delay time.Time, job queue.Job, args []queue.Arg, queue string) error {
 	payload, err := r.jobToGob(job.Signature(), args)
 	if err != nil {
 		return err
--- service_provider.go
+++ service_provider.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/goravel/framework/contracts/foundation"
+	"github.com/goravel/framework/errors"
 )
 
 const (
@@ -21,10 +22,26 @@ func (receiver *ServiceProvider) Register(app foundation.Application) {
 	App = app
 
 	app.BindWith(CacheBinding, func(app foundation.Application, parameters map[string]any) (any, error) {
-		return NewCache(context.Background(), app.MakeConfig(), parameters["store"].(string))
+		config := app.MakeConfig()
+		if config == nil {
+			return nil, errors.ConfigFacadeNotSet.SetModule(errors.ModuleCache)
+		}
+
+		return NewCache(context.Background(), config, parameters["store"].(string))
 	})
+
 	app.BindWith(QueueBinding, func(app foundation.Application, parameters map[string]any) (any, error) {
-		return NewQueue(context.Background(), app.MakeConfig(), app.MakeQueue(), parameters["connection"].(string))
+		config := app.MakeConfig()
+		if config == nil {
+			return nil, errors.ConfigFacadeNotSet.SetModule(errors.ModuleQueue)
+		}
+
+		queue := app.MakeQueue()
+		if queue == nil {
+			return nil, errors.QueueFacadeNotSet.SetModule(errors.ModuleQueue)
+		}
+
+		return NewQueue(context.Background(), config, queue, app.GetJson(), parameters["connection"].(string))
 	})
 	app.BindWith(SessionBinding, func(app foundation.Application, parameters map[string]any) (any, error) {
 		return NewSession(context.Background(), app.MakeConfig(), parameters["driver"].(string))
