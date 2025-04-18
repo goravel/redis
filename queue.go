54dbf1307c7d12555667772eb8c4641519dde7ee
Bowen <hwbrzzl@gmail.com>
2025-04-18 19:33:43 +0800

    add tests

--- queue.go
+++ queue.go
@@ -156,7 +156,7 @@ func (r *Queue) taskToJson(task contractsqueue.Task) ([]byte, error) {
 	}
 
 	t := Task{
-		Uuid:  task.Uuid,
+		Uuid:  task.UUID,
 		Job:   job,
 		Chain: chain,
 	}
@@ -209,7 +209,7 @@ func (r *Queue) jsonToTask(payload string) (contractsqueue.Task, error) {
 	}
 
 	return contractsqueue.Task{
-		Uuid:  task.Uuid,
+		UUID:  task.Uuid,
 		Jobs:  jobs,
 		Chain: chain,
 	}, nil
