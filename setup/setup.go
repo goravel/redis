ea3e2dcfbdf6e39b2050c90b5f21e306e191561a
Bowen <hwbrzzl@gmail.com>
2026-08-04 16:00:02 +0800

    refactor: use literal retry_after default in setup config

--- setup/setup.go
+++ setup/setup.go
@@ -33,7 +33,7 @@ func main() {
         "driver": "custom",
         "connection": "default",
         "queue": "default",
-        "retry_after": config.Env("REDIS_QUEUE_RETRY_AFTER", 60),
+        "retry_after": 60,
         "via": func() (queue.Driver, error) {
             return redisfacades.Queue("redis") // The ` + "`redis`" + ` value is the key of ` + "`connections`" + `
         },
@@ -66,7 +66,6 @@ func main() {
 REDIS_HOST=127.0.0.1
 REDIS_PASSWORD=
 REDIS_PORT=6379
-REDIS_QUEUE_RETRY_AFTER=60
 `
 
 	setup.Install(
